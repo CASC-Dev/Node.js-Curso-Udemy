0b9f2b0f7af9b65771852e9e04633e2a0ff1fe59
csonorajr <carlos.sonora.jr@gmail.com>
2018-11-13 23:40:10 -0600

    Últimas lecciones, archivos nuevos

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -7,13 +7,20 @@ process.env.PORT = process.env.PORT || 3000;
 //================================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //================================================
+//  Vencimiento del token
+//================================================
+process.env.CADUCIDAD_TOKEN = (60 * 60 * 24 * 30);
+//================================================
+//  SEED de autenticación
+//================================================
+process.env.SEED = process.env.SEED || 'Clave-secreta-0-s33d-desarrollo!';
+//================================================
 //  Base de datos
 //================================================
 let urlDB;
-if(process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb: //localhost:27017/cafe';
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://casonorac:N0d3CoUrSe89uD3mY@ds145573.mlab.com:45573/cafe'
+    urlDB = process.env.MONGO_URI;
 }
-process.env.URLDB = urlDB; 
-
+process.env.URLDB = urlDB;
--- Old/server/server.js
+++ Old/server/server.js
@@ -8,13 +8,14 @@ const app = express();
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
 app.use(bodyParser.json());
-//Configuración global de rutas
-app.use(require('./routes/index'));
+app.use(require('./routes/usuario'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
-    if (err) throw err;
+    if(err) throw err;
+
     console.log('Base de datos Online');
 });
 
