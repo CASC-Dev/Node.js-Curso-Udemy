c0a95d5d9106414a4556d831739c4cd4fed689d0
Carlos Antonio Sonora <sonorac@mx1.ibm.com>
2018-12-26 17:41:20 -0600

    Adding new structure

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -43,7 +43,7 @@ app.post('/login', (req, res) => {
         res.json({
             ok: true,
             usuario: usuarioDB,
-            token: token
+            token
         });
     });
 });
