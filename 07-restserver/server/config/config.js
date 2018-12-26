9bfac4f045d3ddc90d9378a15f20a7aceadc9fa1
Carlos Antonio Sonora <sonorac@mx1.ibm.com>
2018-12-26 12:12:39 -0600

    New try

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -19,11 +19,7 @@ process.env.SEED = process.env.SEED || 'Clave-secreta-0-s33d-desarrollo!';
 //================================================
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
-<<<<<<< HEAD
-    urlDB = 'mongodb: //localhost:27017/cafe';
-=======
     urlDB = 'mongodb://localhost:27017/cafe';
->>>>>>> 0c14faa1492c6db6b8ac8add4392e3a255a37627
 } else {
     urlDB = process.env.MONGO_URI;
 }
